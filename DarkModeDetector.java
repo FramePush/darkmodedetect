a3c863c509db91f04bc30221f2cc41e98c497538
Brian Turner <brianturner@framepush.com>
2019-11-30 17:10:26 -0500

    API level checks on android
    Check to make sure running on Android Q before checking ui mode

--- DarkModeDetector.java
+++ DarkModeDetector.java
@@ -2,9 +2,14 @@ package com.framepush.darkmodedetect;
 
 import android.view.ContextThemeWrapper;
 import android.content.res.Configuration;
+import android.os.Build;
 
 public class DarkModeDetector {
     public static int getCurrentMode(ContextThemeWrapper ctw) {
+        if (Build.VERSION.SDK_INT < 29) {  // Need minimum android Q
+            return 0;  // Unspecified
+        }
+
         switch (ctw.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) {
             case Configuration.UI_MODE_NIGHT_YES:
                 return 2;
@@ -12,6 +17,6 @@ public class DarkModeDetector {
                 return 1;
         }
         
-        return 0;
+        return 0;  // Unspecified
     }
 }
